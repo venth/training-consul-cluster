d6c886df82321e903d1bda62e4f5faf15a99408a
venth <artur.krysiak.warszawa@gmail.com>
2016-10-17 18:40:42 +0200

    introduced consul aware service

---
+++ consul-spring-cloud-service/src/main/java/org/venth/training/consul/Application.java
@@ -0,0 +1,31 @@
+package org.venth.training.consul;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Venth on 17/10/2016
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    @RequestMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
